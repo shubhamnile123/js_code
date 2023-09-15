505590127bc79802fc00412799c2255617a0e2c4
Shubham Nile <71750513+shubhamnile123@users.noreply.github.com>
2023-09-15 01:56:02 +0000

    Datatypes Conversioin

---
+++ .devcontainer/js_basics/03_conversionOfDatatypes.js
@@ -0,0 +1,11 @@
+//let score="shubham"
+
+//console.log(typeof score);
+
+//"33"=>33
+//"33abc"=>NaN
+//true =>1;false =>0
+
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
