a8b9482c249d6a2f03175e06f564eba9d34435e2
Shubham Nile <71750513+shubhamnile123@users.noreply.github.com>
2023-09-18 03:52:16 +0000

    Destructuring JSON API

---
+++ .devcontainer/js_basics/12_destructreJSONAPI.js
@@ -0,0 +1,25 @@
+const course = {
+    courseName : "js in Hindi",
+    price  : "999",
+    courseInstructor : "shubham"
+}
+
+const {courseInstructor  :  instructor} = course  // destructuring
+
+console.log(instructor);
+
+
+//JSON
+
+// {
+//     "name" : "shubham",
+//     "courseName" : "JS in hindi",
+//     "price" : "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
